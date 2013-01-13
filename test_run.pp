644de96845b0421367b630144d2f00cb163c9e63
jokerswild663 <jokerswild663@gmail.com>
2013-01-12 21:43:53 -0800

    added messages

--- test_run.pp
+++ test_run.pp
@@ -4,12 +4,22 @@ file {'testing':
 	path	=> '/home/joker/puppet_catch/testfile',
 	ensure	=> present,
 	mode 	=> 0640,
-	content	=> "I am the first of my kind"
+	content	=> "I am the first of my kind",
+	before 	=> Notify['first']
 }
 
 file {'next':
 	path	=> '/home/joker/puppet_catch/the_next',
 	ensure	=> present,
 	mode 	=> 0640,
-	content	=> "I am the younger sibling"
+	content	=> "I am the younger sibling",
+	before	=> Notify['second']
+}
+
+notify{'first':
+	message	=> "the first file is being deployed"
+}
+
+notify{'second':
+	message => "the second file is being deployed"
 }
